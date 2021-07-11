29e2f8fbedf97cb640cc8cb2b912a53e411622c7
Andrew Arrow <oneone@gmail.com>
2021-07-11 07:48:40 -0700

    tcp connections to 17000

--- lib/peer.go
+++ lib/peer.go
@@ -3,8 +3,20 @@ package lib
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func Connect(ip net.IP) {
 	fmt.Println("connecting to peer", ip)
+	netAddr := net.TCPAddr{
+		IP:   ip,
+		Port: 17000,
+	}
+	fmt.Println(netAddr)
+	conn, err := net.DialTimeout(netAddr.Network(), netAddr.String(), 30*time.Second)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(conn)
 }
